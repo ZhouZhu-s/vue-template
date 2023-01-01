2d669fef52de0b19627a2d0d7d4b9ba2ee09de2c
letconstvar <1985225667@qq.com>
2023-01-01 18:18:34 +0800

    feat: 新增 useList 和 useSelectOptions 函数示例

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 // This starter template is using Vue 3 <script setup> SFCs
 import { postLoginApi } from '@/api/login';
 import { useI18n } from 'vue-i18n';
+import { useSelectOptions } from '@/hooks/useSelectOptions';
+import useList from '@/hooks/useList';
 
 const { t } = useI18n();
 
@@ -13,10 +15,63 @@ const test = async () => {
   });
   console.log(results);
 };
+
+interface Fruit {
+  key: number;
+  label: string;
+  value: number;
+}
+function getRemoteData() {
+  return new Promise<Fruit[]>((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          key: 1,
+          label: '苹果',
+          value: 1,
+        },
+        {
+          key: 2,
+          label: '香蕉',
+          value: 2,
+        },
+        {
+          key: 3,
+          label: '橘子',
+          value: 3,
+        },
+      ]);
+    }, 3000);
+  });
+}
+
+/**
+ * eg: useList
+ */
+class FilterOptions {
+  label = undefined;
+  date = [];
+}
+interface FilterOptionType {
+  label: undefined | string;
+  date: Array<any>;
+}
+const { list, loading, getListData, filterOptions } = useList<
+  Fruit,
+  FilterOptionType
+>(getRemoteData, FilterOptions);
+
+/**
+ * eg: useSelectOptions
+ */
+const selectBind = useSelectOptions({
+  apiFun: getRemoteData,
+});
 </script>
 
 <template>
   <span @click="test()">{{ t('date') }}</span>
+  <a-select v-bind="selectBind" style="width: 200px"></a-select>
 </template>
 
 <style scoped></style>
