17e7ec228642ccdeb89077777a869faa70e779a0
letconstvar <1985225667@qq.com>
2023-01-28 15:26:37 +0800

    feat: remove empty attribute

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { RemoveNull } from './types';
+
 export const unique = (function () {
   let num = 1;
   return {
@@ -6,3 +8,15 @@ export const unique = (function () {
     },
   };
 })();
+
+/**
+ * 移除对象只为空的属性
+ */
+export const removeEmpty = <T extends object>(obj: T): RemoveNull<T> => {
+  const nullValue = [undefined, null, ''];
+  return Object.fromEntries(
+    Object.entries(obj)
+      .filter(([_, v]) => !nullValue.includes(v))
+      .map(([k, v]) => [k, v === Object(v) ? removeEmpty(v) : v])
+  ) as RemoveNull<T>;
+};
