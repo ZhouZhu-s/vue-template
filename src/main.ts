e1bc7ca14745fa82484813f41c115f3c4dbc936d
letconstvar <1985225667@qq.com>
2023-01-31 14:07:29 +0800

    feat: PerfectScrollbar

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue';
 import 'ant-design-vue/dist/antd.css';
 import App from './App.vue';
 import i18n from './locales/index';
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
 
-createApp(App).use(i18n).mount('#app');
+createApp(App).use(i18n).use(PerfectScrollbar).mount('#app');
